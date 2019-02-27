ec2590ce3dd645c851c02920a58b91eb482421db
Benjamin Villalonga <benjaminvillalonga@gmail.com>
2019-02-26 23:45:14 -0800

    Reading circuit from file is working well, up to reorderings. It is competitive with MKLTensor read_circuit.

--- read_circuit_no_mkl.h
+++ read_circuit_no_mkl.h
@@ -38,12 +38,16 @@ const double _INV_SQRT_2 = 1./_SQRT_2;
 const int DIM = 2;
 const vector<string> _ALPHABET({"a","b","c","d","e","f","g","h","i","j",
                                 "k","l","m","n","o","p","q","r","s","t",
-                                "u","v","w","x","y","z"});
+                                "u","v","w","x","y","z",
+                                "A","B","C","D","E","F","G","H","I","J",
+                                "K","L","M","N","O","P","Q","R","S","T",
+                                "U","V","W","X","Y","Z"});
 
 // I've flipped the order of the indexes due to TALSH taking Fortran-like
 // storage
 const unordered_map<string,vector<s_type>> _GATES_DATA({
   // Deltas.
+  {"I", vector<s_type>({1.0, 0.0, 0.0, 1.0})}, // Used to reorder (while mult.)
   {"delta_0", vector<s_type>({1.0, 0.0})},
   {"delta_1", vector<s_type>({0.0, 1.0})},
   // For one-qubit gates, the first index is output an second is input.
@@ -394,8 +398,12 @@ void google_circuit_file_to_grid_of_tensors(string filename, int I, int J,
     ++idx; // Move in B only.
   }
 
-  
+
   // Contract each list of tensors onto a single one
+  // Reuse pointer to data for all iterations
+  vector<s_type *> data(2);
+  data[0] = new s_type[(int)pow(2,20)];
+  data[1] = new s_type[(int)pow(2,20)];
   for (int q=0; q<num_qubits; ++q)
   {
     vector<int> i_j = _q_to_i_j(q, J);
@@ -404,396 +412,93 @@ void google_circuit_file_to_grid_of_tensors(string filename, int I, int J,
     { continue; }
     // Rank of the delta tensor is 1
     int current_rank(1), new_tensor_rank, new_rank;
+    string pattern_D(""), pattern_L, pattern_R;
+    //cout << i << " " << j << endl;
+    int idx = 0;
     // Assume there is at least 2 tensors per world-line
-    cout << i << " " << j << endl;
     for (int k=1; k<grid_of_lists_of_tensors[i][j].size(); ++k)
     {
       new_tensor_rank = grid_of_lists_of_tensors[i][j][k].getRank();
       new_rank = current_rank + new_tensor_rank - 2;
-      current_rank = new_rank;
-    }
-    cout << endl;
+      pattern_R = pattern_from_vector(vector<string>(_ALPHABET.cbegin(),
+                                             _ALPHABET.cbegin()+current_rank));
+      if (current_rank>1)
+      {
+        pattern_D = pattern_from_vector(vector<string>(_ALPHABET.cbegin()+1,
+                                             _ALPHABET.cbegin()+current_rank));
+      }
+      else
+        pattern_D = "";
 
-    /*
-    if (grid_of_lists_of_tensors[i][j][1].getRank()>2) continue;
-    s_type * data = new s_type[DIM];
-    talsh::Tensor P({DIM}, data);
-    data = NULL;
-    TensContraction contraction("D(b)+=L(b,a)*R(a)",
-                                &P,
-                                &grid_of_lists_of_tensors[i][j][1],
-                                &grid_of_lists_of_tensors[i][j][0]);
-    int errc = contraction.execute(DEV_HOST,0);
-    assert(errc==TALSH_SUCCESS);
-    assert(contraction.sync(DEV_HOST,0));
-    */
+      if (new_tensor_rank==1)
+      {
+        pattern_L = _ALPHABET[0];
+      }
+      else if (new_tensor_rank==2)
+      {
+        if (pattern_D=="")
+          pattern_D = _ALPHABET[current_rank];
+        else
+          pattern_D = _ALPHABET[current_rank] + "," +  pattern_D;
+        pattern_L = _ALPHABET[current_rank] + "," + _ALPHABET[0];
+      }
+      else if (new_tensor_rank==3)
+      {
+        if (pattern_D=="")
+        {
+          pattern_D = _ALPHABET[current_rank+1] + "," +
+                      _ALPHABET[current_rank];
+        }
+        else
+        {
+          pattern_D = _ALPHABET[current_rank+1] + "," +
+                      _ALPHABET[current_rank] + "," +  pattern_D;
+        }
+        pattern_L = _ALPHABET[current_rank+1] + "," +
+                    _ALPHABET[current_rank] + "," + _ALPHABET[0];
+      }
+      else
+        cout << "Some gate is not properly decomposed into tensors!" << endl;
+      if (pattern_D[pattern_D.length()]==',')
+        pattern_D.erase(pattern_D.length());
+      string pattern = "D("+pattern_D+")+=L("+pattern_L+")*R("+pattern_R+")";
+      vector<int> dims_S(current_rank,DIM);
+      vector<int> dims_T(new_rank,DIM);
+      talsh::Tensor S(dims_S, data[-idx+1]);
+      talsh::Tensor T(dims_T, data[idx]);
+      //cout << k << " " << pattern << endl;
+      if (k==1)
+      {
+        TensContraction contraction(pattern, &T,
+                                         &grid_of_lists_of_tensors[i][j][1],
+                                         &grid_of_lists_of_tensors[i][j][0]);
+        int errc = contraction.execute(DEV_HOST,0);
+        assert(errc==TALSH_SUCCESS);
+        assert(contraction.sync(DEV_HOST,0));
+      }
+      else
+      {
+        TensContraction contraction(pattern, &T,
+                                         &grid_of_lists_of_tensors[i][j][k],
+                                         &S);
+        int errc = contraction.execute(DEV_HOST,0);
+        assert(errc==TALSH_SUCCESS);
+        assert(contraction.sync(DEV_HOST,0));
+      }
+      current_rank = new_rank;
+      idx = -idx + 1;
+    }
+    for (auto v : data)
+      v = NULL;
   }
+
+  // Allocate memory for each q. Try using malloc(). Then reorder (remember we
+  // have a grid of bonds to help with this step.
 
   // Reorder data by multiplying times the identity
 
-  /*
-  string line;
-  while(getline(io, line)) if(line.size() && line[0] != '#') {// Avoid comments
-    stringstream ss(line);
-    // The first element is the cycle
-    ss >> cycle;
-    // The second element is the gate
-    ss >> gate;
-    // Get the first position
-    ss >> q1;
-    // Get the second position in the case
-    if (gate=="cz") ss >> q2;
-    else q2 = -1;
-
-    // Get i, j and super_cycle
-    i_j_1 = _q_to_i_j(q1, J);
-    if (q2>=0)
-    {
-      i_j_2 = _q_to_i_j(q2, J);
-    }
-    super_cycle = (cycle - 1) / SUPER_CYCLE_DEPTH;
-
-    // Fill in one-qubit gates.
-    if (q2<0 && cycle>0 && cycle<=SUPER_CYCLE_DEPTH*K)
-    {
-      if (find(off.begin(),off.end(),
-               vector<int>({i_j_1[0],i_j_1[1]}))!=off.end())
-      {
-        continue;
-      }
-      string input_index = "t"
-              + to_string(counter_group[i_j_1[0]][i_j_1[1]][super_cycle]);
-      string output_index = "t"
-              + to_string(counter_group[i_j_1[0]][i_j_1[1]][super_cycle] + 1);
-      ++counter_group[i_j_1[0]][i_j_1[1]][super_cycle];
-      grid_of_groups_of_tensors[i_j_1[0]][i_j_1[1]][super_cycle].push_back(
-        MKLTensor({input_index,output_index}, {2,2}, _GATES_DATA.at(gate)));
-    }
-    if (q2>=0 && cycle>0 && cycle<=SUPER_CYCLE_DEPTH*K)
-    {
-      if (find(off.begin(),off.end(),
-               vector<int>({i_j_1[0],i_j_1[1]}))!=off.end()
-          ||
-          find(off.begin(),off.end(),
-               vector<int>({i_j_2[0],i_j_2[1]}))!=off.end())
-      {
-        continue;
-      }
-      string input_index_1 = "t"
-              + to_string(counter_group[i_j_1[0]][i_j_1[1]][super_cycle]);
-      string output_index_1 = "t"
-              + to_string(counter_group[i_j_1[0]][i_j_1[1]][super_cycle] + 1);
-      string virtual_index =
-                      "("+to_string(i_j_1[0])+","+to_string(i_j_1[1])+","
-                         +to_string(super_cycle)+"),("+to_string(i_j_2[0])+","
-                         +to_string(i_j_2[1])+","+to_string(super_cycle)+")";
-      string input_index_2 = "t"
-              + to_string(counter_group[i_j_2[0]][i_j_2[1]][super_cycle]);
-      string output_index_2 = "t"
-              + to_string(counter_group[i_j_2[0]][i_j_2[1]][super_cycle] + 1);
-      ++counter_group[i_j_1[0]][i_j_1[1]][super_cycle];
-      ++counter_group[i_j_2[0]][i_j_2[1]][super_cycle];
-      grid_of_groups_of_tensors[i_j_1[0]][i_j_1[1]][super_cycle].push_back(
-                      MKLTensor({input_index_1,virtual_index,output_index_1},
-                                {2,2,2}, _GATES_DATA.at("cz_q1")));
-      grid_of_groups_of_tensors[i_j_2[0]][i_j_2[1]][super_cycle].push_back(
-                      MKLTensor({input_index_2,virtual_index,output_index_2},
-                                {2,2,2}, _GATES_DATA.at("cz_q2")));
-    }
-  }
-  // Insert Hadamards and deltas to last layer.
-  idx = 0;
-  for (int q=0; q<num_qubits; ++q)
-  {
-    vector<int> i_j = _q_to_i_j(q, J);
-    int i = i_j[0], j = i_j[1];
-    int k = K-1;
-    if (find(off.begin(),off.end(),
-             vector<int>({i,j}))!=off.end())
-    { continue; }
-    string last_index = "t"+to_string(counter_group[i][j][k]);
-    grid_of_groups_of_tensors[i][j][k].push_back(
-                    MKLTensor({"th",last_index}, {2,2}, _GATES_DATA.at("h")));
-    if (find(A.begin(),A.end(),vector<int>({i,j}))!=A.end())
-    { continue; }
-    string delta_gate = (final_conf_B[idx]=='0')?"delta_0":"delta_1";
-    grid_of_groups_of_tensors[i][j][k].push_back(
-                    MKLTensor({"th"}, {2}, _GATES_DATA.at(delta_gate)));
-    idx += 1; // Move in B only.
-  }
-
-  // Contracting each group of gates into a single tensor.
-  for (int i=0; i<I; ++i) for (int j=0; j<J; ++j) for (int k=0; k<K; ++k)
-  {
-    if (find(off.begin(),off.end(), vector<int>({i,j}))!=off.end())
-    {
-      continue;
-    }
-
-    vector<MKLTensor> & group = grid_of_groups_of_tensors[i][j][k];
-    vector<MKLTensor> group_containers(SUPER_CYCLE_DEPTH+2, // +2 for d and H.
-                                       MKLTensor({""},{(int)pow(DIM,6)}));
-    group_containers[0] = group[0];
-    int t=1;
-    for (t=1; t<group.size(); ++t)
-    {
-      multiply(group_containers[t-1], group[t], group_containers[t], scratch);
-    }
-    grid_of_tensors[i][j][k] = group_containers[t-1];
-  }
-
-  // Rename "t..." indices.
-  for (int i=0; i<I; ++i) for (int j=0; j<J; ++j) for (int k=0; k<K; ++k)
-  {
-    if (find(off.begin(),off.end(),vector<int>({i,j}))!=off.end())
-    { continue; }
-    if (k>0)
-    {
-      string new_first_index = "("+to_string(i)+","+to_string(j)+","
-                               +to_string(k-1)+"),("+to_string(i)+","
-                               +to_string(j)+","+to_string(k)+")";
-      grid_of_tensors[i][j][k].rename_index("t0", new_first_index);
-    }
-    if (k<K-1)
-    {
-      string last_index = "t"+to_string(counter_group[i][j][k]);
-      string new_last_index = "("+to_string(i)+","+to_string(j)+","
-                              +to_string(k)+"),("+to_string(i)+","
-                              +to_string(j)+","+to_string(k+1)+")";
-      grid_of_tensors[i][j][k].rename_index(last_index, new_last_index);
-    }
-    if (k==K-1 && find(A.begin(),A.end(),vector<int>({i,j}))!=A.end())
-    {
-      string last_index = "th";
-      string new_last_index = "("+to_string(i)+","+to_string(j)+"),(o)";
-      grid_of_tensors[i][j][k].rename_index(last_index, new_last_index);
-    }
-  }
-  */
 }
 
 
-/**
-* Contracts a 3D grid of tensors onto a 2D grid of tensors, contracting
-* in the time (thrid) direction, and renaming the indices accordingly.
-* @param grid_of_tensors_3D reference to a
-* vector<vector<vector<MKLTensor>>> with the 3D grid of tensors. It must be a
-* grid dimensionswise. The typical names for the indices in a grid is assumed.
-* @param grid_of_tensors_2D reference to a vector<vector<MKLTensor>> where the
-* 2D grid of tensors will be stored. The typical names for the indices will
-* be used.
-* @param A vector<vector<int>> with the coords. of the qubits in A.
-* @param off vector<vector<int>> with the coords. of the qubits turned off.
-* @param scratch pointer to s_type array with enough space for all scratch
-* work.
-*/
-/*
-void grid_of_tensors_3D_to_2D(
-      vector<vector<vector<MKLTensor>>> & grid_of_tensors_3D,
-      vector<vector<MKLTensor>> & grid_of_tensors_2D,
-      vector<vector<int>> A, vector<vector<int>> off, s_type * scratch)
-{
-  // Get dimensions and super_dim = DIM^k.
-  const int I = grid_of_tensors_3D.size();
-  const int J = grid_of_tensors_3D[0].size();
-  const int K = grid_of_tensors_3D[0][0].size();
-  const int super_dim = (int)pow(DIM,K);
-
-  // Contract vertically and fill grid_of_tensors_2D.
-  for (int i=0; i<I; ++i) for (int j=0; j<J; ++j)
-  {
-    if (find(off.begin(),off.end(),vector<int>({i,j}))!=off.end())
-    { continue; }
-
-    vector<MKLTensor> group_containers;
-    if (find(A.begin(),A.end(),vector<int>({i,j}))!=A.end())
-    {
-      group_containers = vector<MKLTensor>(K-1,
-                                MKLTensor({""}, {(int)pow(super_dim,4)*DIM}));
-    }
-    else
-    {
-      group_containers = vector<MKLTensor>(K-1,
-                                MKLTensor({""}, {(int)pow(super_dim,4)}));
-    }
-
-    if (K==1)
-    {
-      group_containers[0] = grid_of_tensors_3D[i][j][0];
-    }
-    else
-    {
-      multiply(grid_of_tensors_3D[i][j][0], grid_of_tensors_3D[i][j][1],
-               group_containers[0], scratch);
-      for (int k=1; k<K-1; ++k)
-      {
-        multiply(group_containers[k-1], grid_of_tensors_3D[i][j][k+1],
-                 group_containers[k], scratch);
-      }
-    }
-    grid_of_tensors_2D[i][j] = group_containers[K-2];
-  }
-
-  // Reorder and bundle.
-  for (int i=0; i<I; ++i) for (int j=0; j<J; ++j)
-  {
-    if (find(off.begin(),off.end(),vector<int>({i,j}))!=off.end())
-    { continue; }
-
-    vector<string> ordered_indices_3D;
-    vector<string> indices_2D;
-    string index_name;
-    if (i>0 && find(off.begin(),off.end(),vector<int>({i-1,j}))==off.end())
-    {
-      for (int k=0; k<K; ++k)
-      {
-        index_name = "("+to_string(i-1)+","+to_string(j)+","+to_string(k)+"),("+
-                     to_string(i)+","+to_string(j)+","+to_string(k)+")";
-        ordered_indices_3D.push_back(index_name);
-      }
-      index_name = "("+to_string(i-1)+","+to_string(j)+"),("
-                   +to_string(i)+","+to_string(j)+")";
-      indices_2D.push_back(index_name);
-    }
-    if (j>0 && find(off.begin(),off.end(),vector<int>({i,j-1}))==off.end())
-    {
-      for (int k=0; k<K; ++k)
-      {
-        index_name = "("+to_string(i)+","+to_string(j-1)+","+to_string(k)+"),("+
-                     to_string(i)+","+to_string(j)+","+to_string(k)+")";
-        ordered_indices_3D.push_back(index_name);
-      }
-      index_name = "("+to_string(i)+","+to_string(j-1)+"),("
-                   +to_string(i)+","+to_string(j)+")";
-      indices_2D.push_back(index_name);
-    }
-    if (i<I-1 && find(off.begin(),off.end(),vector<int>({i+1,j}))==off.end())
-    {
-      for (int k=0; k<K; ++k)
-      {
-        index_name = "("+to_string(i)+","+to_string(j)+","+to_string(k)+"),("+
-                     to_string(i+1)+","+to_string(j)+","+to_string(k)+")";
-        ordered_indices_3D.push_back(index_name);
-      }
-      index_name = "("+to_string(i)+","+to_string(j)+"),("
-                   +to_string(i+1)+","+to_string(j)+")";
-      indices_2D.push_back(index_name);
-    }
-    if (j<J-1 && find(off.begin(),off.end(),vector<int>({i,j+1}))==off.end())
-    {
-      for (int k=0; k<K; ++k)
-      {
-        index_name = "("+to_string(i)+","+to_string(j)+","+to_string(k)+"),("+
-                     to_string(i)+","+to_string(j+1)+","+to_string(k)+")";
-        ordered_indices_3D.push_back(index_name);
-      }
-      index_name = "("+to_string(i)+","+to_string(j)+"),("
-                   +to_string(i)+","+to_string(j+1)+")";
-      indices_2D.push_back(index_name);
-    }
-    if (find(A.begin(),A.end(),vector<int>({i,j}))!=A.end())
-    {
-      index_name = "("+to_string(i)+","+to_string(j)+"),(o)";
-      ordered_indices_3D.push_back(index_name);
-    }
-
-    // Reorder.
-    grid_of_tensors_2D[i][j].reorder(ordered_indices_3D, scratch);
-
-    // Bundle.
-    int max_idx;
-    max_idx = indices_2D.size();
-    for (int idx_num=0; idx_num<max_idx; ++idx_num)
-    {
-      vector<string> indices_to_bundle(ordered_indices_3D.begin()+idx_num*K,
-                                     ordered_indices_3D.begin()+(idx_num+1)*K);
-      grid_of_tensors_2D[i][j].bundle(indices_to_bundle, indices_2D[idx_num]);
-    }
-  }
-
-  // Be proper about pointers.
-  scratch = NULL;
-}
-*/
-
-
-/**
-* Read circuit from file and fill vector of tensors (of gates), vector with 
-* names of the input indices of the tensors and vector with the output indices
-* of the tensors.
-* @param filename string with the name of the circuit file.
-* @param I int with the number of qubits.
-* @param gates reference to a vector<MKLTensor> to be filled with the gates.
-* @param inputs reference to a vector<vector<string>> to be filled with the
-* input indexes of the gates.
-* @param outputs reference to a vector<vector<string>> to be filled with the
-* output indexes of the gates.
-* @param scratch pointer to s_type array with scratch space for operations
-* performed in this function.
-* 
-*/
-/*
-void read_wave_function_evolution(string filename, int I,
-          vector<MKLTensor> & gates, vector<vector<string>> & inputs,
-          vector<vector<string>> & outputs, s_type * scratch)
-{
-  // Open file.
-  auto io = ifstream(filename);
-  assert(io.good() && "Cannot open file.");
-
-  // Gotten from the file.
-  int num_qubits, cycle, q1, q2;
-  string gate;
-  // Useful for plugging into the tensor network:
-  vector<int> i_j_1, i_j_2;
-  int super_cycle;
-
-  // The first element should be the number of qubits
-  io >> num_qubits;
-
-  // Assert for the number of qubits.
-  assert(num_qubits==I && "I must be equal to the number of qubits.");
-
-  string line;
-  while(getline(io, line)) if(line.size() && line[0] != '#') {// Avoid comments
-    stringstream ss(line);
-    // The first element is the cycle
-    ss >> cycle;
-    // The second element is the gate
-    ss >> gate;
-    // Get the first position
-    ss >> q1;
-    // Get the second position in the case
-    if (gate=="cz") ss >> q2;
-    else q2 = -1;
-
-    // Fill in one-qubit gates.
-    if (q2<0)
-    {
-      string input_index = to_string(q1) + ",i";
-      string output_index = to_string(q1) + ",o";
-      gates.push_back(MKLTensor({input_index,output_index},{DIM,DIM},
-                                _GATES_DATA.at(gate)));
-      inputs.push_back({input_index});
-      outputs.push_back({output_index});
-    }
-    if (q2>=0)
-    {
-      string input_index1 = to_string(q1) + ",i";
-      string output_index1 = to_string(q1) + ",o";
-      string input_index2 = to_string(q2) + ",i";
-      string output_index2 = to_string(q2) + ",o";
-      inputs.push_back({input_index1,input_index2});
-      outputs.push_back({output_index1,output_index2});
-      gates.push_back(MKLTensor(
-                    {input_index1,input_index2,output_index1,output_index2},
-                    {DIM,DIM,DIM,DIM},_GATES_DATA.at(gate)));
-    }
-  }
-
-  // Be proper about pointers.
-  scratch = NULL;
-}
-*/
 
 #endif
