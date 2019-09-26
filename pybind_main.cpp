f9897f363970366d33c03acbbabf68d12c745809
alexandrupaler <Sansibar2009>
2019-09-27 00:27:22 +0200

    google format

--- pybind_main.cpp
+++ pybind_main.cpp
@@ -1,6 +1,6 @@
+#include <pybind11/complex.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include <pybind11/complex.h>
 namespace py = pybind11;
 
 #include <omp.h>
@@ -16,101 +16,101 @@ namespace py = pybind11;
 // $ ./qflex.x 11 12 2 0.005 ./circuits/ben_11_16_0.txt \
 //       ./ordering/bristlecone_48.txt ./grid/bristlecone_48.txt
 
-std::vector<std::pair<std::string, std::complex<double>>> simulate(std::vector<std::string> circuit_content,
+std::vector<std::pair<std::string, std::complex<double>>> simulate(
+    std::vector<std::string> circuit_content,
     std::vector<std::string> ordering_content,
-    std::vector<std::string> grid_content)
-{
-    // Reading input.
-    // if (argc < 8) throw std::logic_error("ERROR: Not enough arguments.");
-    int current_arg = 1;
-    qflex::QflexInput input;
+    std::vector<std::string> grid_content) {
+  // Reading input.
+  // if (argc < 8) throw std::logic_error("ERROR: Not enough arguments.");
+  int current_arg = 1;
+  qflex::QflexInput input;
 
-//   input.I = atoi(argv[current_arg++]);
-//   input.J = atoi(argv[current_arg++]);
-//   input.K = atoi(argv[current_arg++]);
-//   input.fidelity = atof(argv[current_arg++]);
+  //   input.I = atoi(argv[current_arg++]);
+  //   input.J = atoi(argv[current_arg++]);
+  //   input.K = atoi(argv[current_arg++]);
+  //   input.fidelity = atof(argv[current_arg++]);
 
-    /*
-        TODO: for the moment, hard coded
-    */
-    input.I = 11;
-    input.J = 12;
-    input.K = 2;
-    input.fidelity = 0.005;
+  /*
+      TODO: for the moment, hard coded
+  */
+  input.I = 11;
+  input.J = 12;
+  input.K = 2;
+  input.fidelity = 0.005;
 
-    // Creating streams for input files.
-//   std::string circuit_filename = std::string(argv[current_arg++]);
-//   auto circuit_data = std::ifstream(circuit_filename);
-//   if (!circuit_data.good()) {
-//     std::cout << "Cannot open circuit data file: " << circuit_filename
-//               << std::endl;
-//     assert(circuit_data.good());
-//   }
-    std::stringstream circuit_stream;
-    for(std::vector<std::string>::size_type i = 0; i < circuit_content.size(); i++)
-    {
-        circuit_stream << circuit_content[i];
-    }
-    input.circuit_data = &circuit_stream;
+  // Creating streams for input files.
+  //   std::string circuit_filename = std::string(argv[current_arg++]);
+  //   auto circuit_data = std::ifstream(circuit_filename);
+  //   if (!circuit_data.good()) {
+  //     std::cout << "Cannot open circuit data file: " << circuit_filename
+  //               << std::endl;
+  //     assert(circuit_data.good());
+  //   }
+  std::stringstream circuit_stream;
+  for (std::vector<std::string>::size_type i = 0; i < circuit_content.size();
+       i++) {
+    circuit_stream << circuit_content[i];
+  }
+  input.circuit_data = &circuit_stream;
 
-//   std::string ordering_filename = std::string(argv[current_arg++]);
-//   auto ordering_data = std::ifstream(ordering_filename);
-//   if (!ordering_data.good()) {
-//     std::cout << "Cannot open ordering data file: " << ordering_filename
-//               << std::endl;
-//     assert(ordering_data.good());
-//   }
-    std::stringstream ordering_stream;
-    for(std::vector<std::string>::size_type i = 0; i < ordering_content.size(); i++)
-    {
-        ordering_stream << ordering_content[i];
-    }
-    input.ordering_data = &ordering_stream;
+  //   std::string ordering_filename = std::string(argv[current_arg++]);
+  //   auto ordering_data = std::ifstream(ordering_filename);
+  //   if (!ordering_data.good()) {
+  //     std::cout << "Cannot open ordering data file: " << ordering_filename
+  //               << std::endl;
+  //     assert(ordering_data.good());
+  //   }
+  std::stringstream ordering_stream;
+  for (std::vector<std::string>::size_type i = 0; i < ordering_content.size();
+       i++) {
+    ordering_stream << ordering_content[i];
+  }
+  input.ordering_data = &ordering_stream;
 
-//   std::string grid_filename = std::string(argv[current_arg++]);
-//   auto grid_data = std::ifstream(grid_filename);
-//   if (!grid_data.good()) {
-//     std::cout << "Cannot open grid data file: " << grid_filename << std::endl;
-//     assert(grid_data.good());
-//   }
-    std::stringstream grid_stream;
-    for(std::vector<std::string>::size_type i = 0; i < grid_content.size(); i++)
-    {
-        grid_stream << grid_content[i];
-    }
-    input.grid_data = &grid_stream;
+  //   std::string grid_filename = std::string(argv[current_arg++]);
+  //   auto grid_data = std::ifstream(grid_filename);
+  //   if (!grid_data.good()) {
+  //     std::cout << "Cannot open grid data file: " << grid_filename <<
+  //     std::endl; assert(grid_data.good());
+  //   }
+  std::stringstream grid_stream;
+  for (std::vector<std::string>::size_type i = 0; i < grid_content.size();
+       i++) {
+    grid_stream << grid_content[i];
+  }
+  input.grid_data = &grid_stream;
 
-    /*
-        TODO: for the moment, the following two are not sent as parameters
-    */
-    // Setting initial and final circuit states.
-    // if (argc > current_arg) {
-    //     input.initial_state = std::string(argv[current_arg++]);
-    // }
-    // if (argc > current_arg) {
-    //     input.final_state_A = std::string(argv[current_arg++]);
-    // }
+  /*
+      TODO: for the moment, the following two are not sent as parameters
+  */
+  // Setting initial and final circuit states.
+  // if (argc > current_arg) {
+  //     input.initial_state = std::string(argv[current_arg++]);
+  // }
+  // if (argc > current_arg) {
+  //     input.final_state_A = std::string(argv[current_arg++]);
+  // }
 
-    // Evaluating circuit.
-    std::vector<std::pair<std::string, std::complex<double>>> amplitudes =
-        qflex::EvaluateCircuit(&input);
+  // Evaluating circuit.
+  std::vector<std::pair<std::string, std::complex<double>>> amplitudes =
+      qflex::EvaluateCircuit(&input);
 
-    // Printing output.
-    std::cout << "This is from C++" << std::endl;
-    for (int c = 0; c < amplitudes.size(); ++c) {
-        const auto &state = amplitudes[c].first;
-        const auto &amplitude = amplitudes[c].second;
-        std::cout << "XXXX" << " --> " << state << ": "
-                << std::real(amplitude) << " " << std::imag(amplitude)
-                << std::endl;
-    }
-    std::cout << std::endl;
+  // Printing output.
+  std::cout << "This is from C++" << std::endl;
+  for (int c = 0; c < amplitudes.size(); ++c) {
+    const auto &state = amplitudes[c].first;
+    const auto &amplitude = amplitudes[c].second;
+    std::cout << "XXXX"
+              << " --> " << state << ": " << std::real(amplitude) << " "
+              << std::imag(amplitude) << std::endl;
+  }
+  std::cout << std::endl;
 
-    return amplitudes;
+  return amplitudes;
 }
 
 PYBIND11_MODULE(qflex, m) {
-    m.doc() = "pybind11 example plugin"; // optional module docstring
+  m.doc() = "pybind11 example plugin";  // optional module docstring
 
-    m.def("simulate", &simulate, "A function which adds two numbers");
+  m.def("simulate", &simulate, "A function which adds two numbers");
 }
