3fabc52fa0e3753e83fb1e1b68e606185bdd419f
Salvatore Mandrà <salvatore.mandra@gmail.com>
2019-10-23 05:55:13 -0700

    Different errors return different error numbers.

--- src/main.cpp
+++ src/main.cpp
@@ -91,12 +91,12 @@ int main(int argc, char** argv) {
   } catch (const std::exception &ex) {
 
     std::cerr << ex.what() << std::endl;
-    return -1;
+    return 1;
 
   } catch (const std::string &msg) {
 
     std::cerr << msg << std::endl;
-    return -1;
+    return 2;
 
   }
 
