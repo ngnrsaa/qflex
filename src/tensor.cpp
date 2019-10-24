595e1738675e3395655eaff009d98e11bee89f7b
Salvatore Mandrà <salvatore.mandra@gmail.com>
2019-10-23 18:20:44 -0700

    OpenMP is now an experimental feature.
    OpenMP in src/tensor.cpp causes memory leaks that need to be fixed.

--- src/tensor.cpp
+++ src/tensor.cpp
@@ -15,7 +15,9 @@
 
 #include "tensor.h"
 
-#include <omp.h>
+#ifdef _OPENMP
+  #include <omp.h>
+#endif
 
 #include <algorithm>
 #include <cassert>
@@ -641,7 +643,11 @@ void Tensor::_right_reorder(const std::vector<std::string>& old_ordering,
     s_type* temp_data = new s_type[dim_right];
 #pragma omp for schedule(static)
     for (int pl = 0; pl < dim_left; ++pl) {
+  #ifdef _OPENMP
       int current_thread = omp_get_thread_num();
+  #else
+      int current_thread = 0;
+  #endif
       int offset = pl * dim_right;
       for (int pr = 0; pr < dim_right; ++pr)
         *(temp_data + pr) = *(_data + offset + pr);
