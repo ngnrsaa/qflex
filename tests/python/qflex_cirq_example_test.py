a2c131b17a9bafeb0d24c93452951349adb1a884
95-martin-orion <40585662+95-martin-orion@users.noreply.github.com>
2019-10-24 09:51:05 -0700

    Reset CWD after testing.
    Also rename from `qflex_circ...` to `qflex_cirq...`

--- tests/python/qflex_cirq_example_test.py
+++ tests/python/qflex_cirq_example_test.py
@@ -6,7 +6,11 @@ from python import qflex_cirq_example as qce
 def test_example():
     # change CWD - the hacky way
     import os
+    init_cwd = os.getcwd()
     os.chdir("../..")
 
     # If it does not crash, then it is fine
     qce.main()
+    
+    # Return CWD to its starting state
+    os.chdir(init_cwd)
