e0eaec7af37363c04a51a5acf41b965440bca21e
Salvatore Mandrà <salvatore.mandra@gmail.com>
2019-11-04 08:56:00 -0800

    Fix format.

--- src/pybind_main.cpp
+++ src/pybind_main.cpp
@@ -1,62 +1,73 @@
 #include "pybind_main.h"
 
-inline PyObject* simulate(PyObject *, PyObject *arg) {
-
+inline PyObject *simulate(PyObject *, PyObject *arg) {
   auto GetStream = [](const auto &data, const std::string &caller) {
     std::stringstream out;
-    for(const auto &line: data)
-      if(PyCPP::Is<PyCPP::String>(line))
+    for (const auto &line : data)
+      if (PyCPP::Is<PyCPP::String>(line))
         out << line << std::endl;
       else
         throw PyCPP::SetError(caller + " must be a list of strings.");
     return out;
   };
 
-  auto LoadData = [&GetStream](const auto &options, const std::string &argument, auto &data) {
-    if(auto w = options.find(PyCPP::String(argument)); w != std::end(options)) {
+  auto LoadData = [&GetStream](const auto &options, const std::string &argument,
+                               auto &data) {
+    if (auto w = options.find(PyCPP::String(argument));
+        w != std::end(options)) {
       const auto &q = std::get<1>(*w);
-      if(PyCPP::Is<PyCPP::Sequence>(q))
+      if (PyCPP::Is<PyCPP::Sequence>(q))
         data.load(GetStream(PyCPP::As<PyCPP::Sequence>(q), argument));
       else
         throw PyCPP::SetError("'" + argument + "' must be a list of strings.");
-    } else if(auto w = options.find(PyCPP::String(argument + "_filename")); w != std::end(options)) {
+    } else if (auto w = options.find(PyCPP::String(argument + "_filename"));
+               w != std::end(options)) {
       const auto &q = std::get<1>(*w);
-      if(PyCPP::Is<PyCPP::String>(q))
+      if (PyCPP::Is<PyCPP::String>(q))
         data.load(PyCPP::As<PyCPP::String>(q));
       else
         throw PyCPP::SetError("'" + argument + "_filename' must be a string.");
     }
   };
 
-  auto LoadStates = [](const auto &options, const std::string &argument, const std::size_t num_active_qubits = 0) {
+  auto LoadStates = [](const auto &options, const std::string &argument,
+                       const std::size_t num_active_qubits = 0) {
     std::vector<std::string> states;
-    if(auto w = options.find(PyCPP::String(argument + "_states")); w != std::end(options)) {
+    if (auto w = options.find(PyCPP::String(argument + "_states"));
+        w != std::end(options)) {
       const auto &q = std::get<1>(*w);
-      if(PyCPP::Is<PyCPP::String>(q)) { 
+      if (PyCPP::Is<PyCPP::String>(q)) {
         states.push_back(PyCPP::As<PyCPP::String>(q));
-      } else if(PyCPP::Is<PyCPP::Sequence>(q)) {
-        for(const auto &x: PyCPP::As<PyCPP::Sequence>(q))
-          if(PyCPP::Is<PyCPP::String>(x)) states.push_back(PyCPP::As<PyCPP::String>(x));
-          else throw PyCPP::SetError("states must be strings.");
-      } else throw PyCPP::SetError(argument + "_states must be a list of strings.");
-    } else if(auto w = options.find(PyCPP::String(argument + "_state")); w != std::end(options)) {
+      } else if (PyCPP::Is<PyCPP::Sequence>(q)) {
+        for (const auto &x : PyCPP::As<PyCPP::Sequence>(q))
+          if (PyCPP::Is<PyCPP::String>(x))
+            states.push_back(PyCPP::As<PyCPP::String>(x));
+          else
+            throw PyCPP::SetError("states must be strings.");
+      } else
+        throw PyCPP::SetError(argument + "_states must be a list of strings.");
+    } else if (auto w = options.find(PyCPP::String(argument + "_state"));
+               w != std::end(options)) {
       const auto &q = std::get<1>(*w);
-      if(PyCPP::Is<PyCPP::String>(q)) { 
+      if (PyCPP::Is<PyCPP::String>(q)) {
         states.push_back(PyCPP::As<PyCPP::String>(q));
-      } else throw PyCPP::SetError("states must be strings.");
+      } else
+        throw PyCPP::SetError("states must be strings.");
     } else {
-      if(argument == "initial") states.push_back(std::string(num_active_qubits, '0'));
-      else states.push_back("");
+      if (argument == "initial")
+        states.push_back(std::string(num_active_qubits, '0'));
+      else
+        states.push_back("");
     }
     return states;
   };
 
   try {
-
     // The passed argument must be a dictionary
     const auto v_options = PyCPP::Parse(arg);
-    if(not PyCPP::Is<PyCPP::Map>(v_options[0])) {
-      throw PyCPP::SetError("Simulation takes only one argument and it must be a dictionary");
+    if (not PyCPP::Is<PyCPP::Map>(v_options[0])) {
+      throw PyCPP::SetError(
+          "Simulation takes only one argument and it must be a dictionary");
     }
     const PyCPP::Map &options = PyCPP::As<PyCPP::Map>(v_options[0]);
 
@@ -67,33 +78,34 @@ inline PyObject* simulate(PyObject *, PyObject *arg) {
     LoadData(options, "grid", input.grid);
     LoadData(options, "circuit", input.circuit);
     LoadData(options, "ordering", input.ordering);
-    const auto initial_states = LoadStates(options, "initial", input.circuit.num_active_qubits);
+    const auto initial_states =
+        LoadStates(options, "initial", input.circuit.num_active_qubits);
     const auto final_states = LoadStates(options, "final");
 
     // Define container for amplitudes
-    std::vector<std::pair<std::string, std::vector<std::pair<std::string, std::complex<double>>>>> amplitudes;
+    std::vector<std::pair<
+        std::string, std::vector<std::pair<std::string, std::complex<double>>>>>
+        amplitudes;
 
-    auto run_simulation = [&](const std::string &initial_state = "", const std::string &final_state = "") {
+    auto run_simulation = [&](const std::string &initial_state = "",
+                              const std::string &final_state = "") {
       input.initial_state = initial_state;
       input.final_state = final_state;
       return EvaluateCircuit(&input);
     };
 
-    for(const auto &is: initial_states) {
-      for(const auto &fs: final_states) {
+    for (const auto &is : initial_states) {
+      for (const auto &fs : final_states) {
         amplitudes.push_back({is, run_simulation(is, fs)});
       }
     }
 
-    if(std::size(initial_states) == 1 and std::size(final_states) == 1)
+    if (std::size(initial_states) == 1 and std::size(final_states) == 1)
       return PyCPP::Build(std::get<1>(amplitudes[0]));
     else
       return PyCPP::Build(amplitudes);
 
-  } catch(...) {
-
+  } catch (...) {
     return nullptr;
-
   }
-
 }
--- src/pybind_main.h
+++ src/pybind_main.h
@@ -3,31 +3,24 @@
 
 #include <Python.h>
 #include "PyCPP/py.h"
-#include "PyCPP/py_parse.h"
 #include "PyCPP/py_build.h"
+#include "PyCPP/py_parse.h"
 #include "input.h"
 
 namespace qflex {
-  extern std::vector<std::pair<std::string, std::complex<double>>> EvaluateCircuit(QflexInput*);
+extern std::vector<std::pair<std::string, std::complex<double>>>
+EvaluateCircuit(QflexInput *);
 }
 
-inline PyObject* simulate(PyObject *, PyObject *q);
+inline PyObject *simulate(PyObject *, PyObject *q);
 
-static PyMethodDef methods[] = {
-    { "simulate", (PyCFunction)simulate, METH_O, "Simulate circuit using qFlex." },
-    { nullptr, nullptr, 0, nullptr }
-};
+static PyMethodDef methods[] = {{"simulate", (PyCFunction)simulate, METH_O,
+                                 "Simulate circuit using qFlex."},
+                                {nullptr, nullptr, 0, nullptr}};
 
-static PyModuleDef module = {
-    PyModuleDef_HEAD_INIT,
-    "qFlex",
-    "qFlex porting to Python.",
-    0,
-    methods
-};
+static PyModuleDef module = {PyModuleDef_HEAD_INIT, "qFlex",
+                             "qFlex porting to Python.", 0, methods};
 
-PyMODINIT_FUNC PyInit_qflex() {
-    return PyModule_Create(&module);
-}
+PyMODINIT_FUNC PyInit_qflex() { return PyModule_Create(&module); }
 
 #endif
