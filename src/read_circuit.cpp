2571dafc82ecf38fe942f1a5e9de49ef04e05421
Salvatore Mandrà <salvatore.mandra@gmail.com>
2019-11-08 22:07:43 -0800

    Check that off qubits are actually within the grid.

--- src/read_circuit.cpp
+++ src/read_circuit.cpp
@@ -366,6 +366,15 @@ void circuit_data_to_tensor_network(
     idx += 1;
   }
 
+  // Check if off contains valid qubits
+  if (off.has_value())
+    for(const auto &w: off.value()) {
+      const auto &x = w[0];
+      const auto &y = w[1];
+      if (x < 0 or x >= I or y < 0 or y >= J)
+        throw ERROR_MSG("Off qubit '(", x, ", ", y, ")' is outside the grid.");
+    }
+
   std::unordered_set<int> used_qubits;
   std::size_t last_cycle{0};
   for (auto gate : circuit.gates) {
