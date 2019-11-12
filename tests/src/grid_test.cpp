5ccd42ec3146dbef215e19fc49cc2dec1eeb546d
Alan Kao <alankao64@gmail.com>
2019-11-12 15:13:43 -0800

    fix format

--- tests/src/grid_test.cpp
+++ tests/src/grid_test.cpp
@@ -7,14 +7,13 @@ namespace qflex {
 namespace {
 
 TEST(GridExceptionTest, InvalidFilenameInput) {
-    QflexGrid grid; 
-    std::string invalid_filename = "invalid.txt";
-    try {
-        grid.load(invalid_filename);
-    } catch (std::string msg) {
-        EXPECT_THAT(msg, testing::HasSubstr("Cannot open grid file: invalid.txt"));
-    }
-
+  QflexGrid grid;
+  std::string invalid_filename = "invalid.txt";
+  try {
+    grid.load(invalid_filename);
+  } catch (std::string msg) {
+    EXPECT_THAT(msg, testing::HasSubstr("Cannot open grid file: invalid.txt"));
+  }
 }
 
 // Invalid grid layout.
@@ -23,14 +22,13 @@ constexpr char kInvalidTestGrid[] = R"(1 1 1
                                        1 1 1)";
 
 TEST(GridExceptionTest, InvalidGridFormat) {
-    std::stringstream stream(kInvalidTestGrid);
-    QflexGrid grid;
-    try {
-        grid.load(stream);
-    } catch (std::string msg) {
-        EXPECT_THAT(msg, testing::HasSubstr("Grid size is inconsistent."));
-    }
-
+  std::stringstream stream(kInvalidTestGrid);
+  QflexGrid grid;
+  try {
+    grid.load(stream);
+  } catch (std::string msg) {
+    EXPECT_THAT(msg, testing::HasSubstr("Grid size is inconsistent."));
+  }
 }
 
 // Grid layout with trailing whitespace.
@@ -66,7 +64,6 @@ TEST(GridTest, ReadValidGrid6x2) {
   EXPECT_EQ(grid.I, 6);
   EXPECT_EQ(grid.J, 2);
 }
-
 
 }  // namespace
 }  // namespace qflex
