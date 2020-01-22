06d664a6b69c7544987d16ab0d4d7c99232f7852
Salvatore Mandrà <salvatore.mandra@gmail.com>
2020-01-21 19:43:51 -0800

    Implementing memory tracking for multiple platform.

--- src/main.cpp
+++ src/main.cpp
@@ -25,10 +25,8 @@ tensor network, CPU-based simulator of large quantum circuits.
     -g,--grid=<grid_filename>              Grid filename.
     -v,--verbosity=<verbosity_level>       Verbosity level [default: 0].
     -m,--memory=<memory_limit>             Memory limit [default: 1GB].)"
-#ifdef __linux__
     "\n"
     R"(    -t,--track-memory=<milliseconds>       If <verbosity_level> > 0, track memory usage [default: 0].)"
-#endif
     "\n"
     R"(    --initial-conf=<initial_conf>          Initial configuration.
     --final-conf=<final_conf>              Final configuration.
@@ -65,7 +63,6 @@ int main(int argc, char** argv) {
       qflex::global::memory_limit = qflex::utils::from_readable_memory_string(
           args["<memory_limit>"].asString());
 
-#ifdef __linux__
     if (static_cast<bool>(args["--track-memory"]))
       qflex::global::track_memory = args["--track-memory"].asLong();
     else if (static_cast<bool>(args["<track_memory_milliseconds>"]))
@@ -80,7 +77,6 @@ int main(int argc, char** argv) {
                 << std::endl;
       qflex::global::track_memory = 100;
     }
-#endif
 
     // Get initial/final configurations
     if (static_cast<bool>(args["--initial-conf"]))
@@ -106,7 +102,6 @@ int main(int argc, char** argv) {
                                     : args["<grid_filename>"].asString();
 
     // Print info on maximum memory
-#ifdef __linux__
     if (qflex::global::verbose > 0)
       std::cerr << "Maximum allowed memory: "
                 << qflex::utils::readable_memory_string(
@@ -119,7 +114,6 @@ int main(int argc, char** argv) {
       ualarm(qflex::global::track_memory * 1e3,
              qflex::global::track_memory * 1e3);
     }
-#endif
 
     // Load circuit
     input.circuit.load(std::ifstream(circuit_filename));
@@ -140,10 +134,8 @@ int main(int argc, char** argv) {
     }
     // If no error is caught, amplitudes will be initialized.
 
-#ifdef __linux__
     if (qflex::global::verbose > 0 && qflex::global::track_memory > 0)
       qflex::memory::print_memory_usage();
-#endif
 
     // Printing output.
     for (std::size_t c = 0; c < amplitudes.size(); ++c) {
--- src/memory.h
+++ src/memory.h
@@ -5,40 +5,65 @@
 #include <unistd.h>
 
 #include <array>
-#include <fstream>
+//#include <fstream>
 #include <iostream>
 
 #include "utils.h"
 
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+  #error
+#elif __APPLE__
+  #include <sys/resource.h>
+  #include <mach/mach.h>
+#elif __unix__
+  #include <sys/time.h>
+  #include <sys/resource.h>
+#endif
+
 namespace qflex::memory {
 
 inline std::array<std::string, 2> get_memory_usage() noexcept {
+
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+
+  #error
+
+#elif __APPLE__
+
+  struct mach_task_basic_info info;
+  mach_msg_type_number_t count = MACH_TASK_BASIC_INFO_COUNT;
+  if (task_info(mach_task_self(), MACH_TASK_BASIC_INFO, (task_info_t)&info, &count) == KERN_SUCCESS)
+    return {utils::readable_memory_string(info.resident_size), utils::readable_memory_string(info.resident_size_max)};
+
+#elif __unix__
+
   if (auto in = std::ifstream("/proc/self/status"); in.good()) {
-    // Skip to the desired line
-    std::string line;
-    for (std::size_t i = 0; i < 17; ++i) std::getline(in, line);
 
-    // Strip everything which is not a number at the beginning of line
-    line = line.substr(line.find_first_of("0123456789"), std::size(line));
+    auto get_value = [](std::string line) {
 
-    // Strip everything after the first space and get memory peak
-    const std::string m_peak = utils::readable_memory_string(
-        std::stod(line.substr(0, line.find(' '))) * 1024);
+      // Strip everything which is not a number at the beginning of line
+      line = line.substr(line.find_first_of("0123456789"), std::size(line));
 
-    // Get next line
-    std::getline(in, line);
+      // Strip everything after the first space and get value
+      return utils::readable_memory_string(std::stod(line.substr(0, line.find(' '))) * 1024);
 
-    // Strip everything which is not a number at the beginning of line
-    line = line.substr(line.find_first_of("0123456789"), std::size(line));
+    };
 
-    // Strip everything after the first space and get memory usage
-    const std::string m_used = utils::readable_memory_string(
-        std::stod(line.substr(0, line.find(' '))) * 1024);
+    // Read file and get values
+    std::string line, m_used, m_peak;
+    while(std::getline(in, line)) {
+      if(line.rfind("VmHWM", 0)) m_peak = get_value(line);
+      else if(line.rfind("VmRSS", 0)) m_used = get_value(line);
+    }
 
     return {m_used, m_peak};
 
-  } else
-    return {std::string("n/a"), std::string("n/a")};
+  } 
+
+#endif
+
+  return {std::string("n/a"), std::string("n/a")};
+
 }
 
 inline void print_memory_usage(int _ = 0) noexcept {
