3fc58958f74fc8a0a008018311d43d852b6c293b
Salvatore Mandrà <salvatore.mandra@gmail.com>
2020-02-24 18:46:57 -0800

    Update Rochester simulation.

--- src/global.h
+++ src/global.h
@@ -9,7 +9,7 @@ namespace qflex::global {
 inline int verbose = 0;
 
 // Max allowed memory (default: 1GB)
-inline std::size_t memory_limit = 1L << 30;
+inline std::size_t memory_limit = 5L << 30;
 
 // Interval to track memory (default: 0)
 inline std::size_t track_memory_seconds = 0;
