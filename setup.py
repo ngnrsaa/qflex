06de9e657f608840212d959dfa195a8f9b33314e
alexandrupaler <alexandrupaler@gmail.com>
2020-02-27 08:21:14 -0800

    minor edits.

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+# Read in requirements
+requirements = open('scripts/requirements.txt').readlines()
+requirements = [r.strip() for r in requirements]
+
 setuptools.setup(
-    name="qflex-cirq-interface", # Replace with your own username
+    name="qflex-cirq", # Replace with your own username
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
+    author="The QFlex Contributors",
     description="A Google Cirq interface for QFlex",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ngnrsaa/qflex",
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache2 License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
+    python_requires=('>=3.6.0'),
+    install_requires=requirements,
+    license='Apache 2',
 )
