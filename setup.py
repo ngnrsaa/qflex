f06ca28589933f872eec5f21acfb8762c5315e41
alexandrupaler <alexandrupaler@gmail.com>
2020-02-27 10:25:32 -0800

    first step towards proper module

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 import setuptools
 
+from setuptools import setup, Extension
+
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 # Read in requirements
 requirements = open('scripts/requirements.txt').readlines()
 requirements = [r.strip() for r in requirements]
+
+class CMakeExtension(Extension):
+
+  def __init__(self, name, sourcedir=''):
+    Extension.__init__(self, name, sources=[])
+    self.sourcedir = os.path.abspath(sourcedir)
 
 setuptools.setup(
     name="qflex-cirq", # Replace with your own username
@@ -15,8 +24,76 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ngnrsaa/qflex",
-    packages=setuptools.find_packages(),
+    packages=['qflexcirq'],
     python_requires=('>=3.6.0'),
     install_requires=requirements,
     license='Apache 2',
+
+    ext_modules=[CMakeExtension('qflexcirq/qflex')],
+    cmdclass=dict(build_ext=CMakeBuild),
+    zip_safe=False,
 )
+
+### Vamsi
+
+
+import os
+import re
+import sys
+import platform
+import subprocess
+
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+from distutils.version import LooseVersion
+
+
+class CMakeBuild(build_ext):
+
+  def run(self):
+    try:
+      out = subprocess.check_output(['cmake', '--version'])
+    except OSError:
+      raise RuntimeError(
+          'CMake must be installed to build the following extensions: ' +
+          ', '.join(e.name for e in self.extensions))
+
+    if platform.system() == 'Windows':
+      cmake_version = LooseVersion(
+          re.search(r'version\s*([\d.]+)', out.decode()).group(1))
+      if cmake_version < '3.1.0':
+        raise RuntimeError('CMake >= 3.1.0 is required on Windows')
+
+    for ext in self.extensions:
+      self.build_extension(ext)
+
+  def build_extension(self, ext):
+    extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+    cmake_args = [
+        '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+        '-DPYTHON_EXECUTABLE=' + sys.executable
+    ]
+
+    cfg = 'Debug' if self.debug else 'Release'
+    build_args = ['--config', cfg]
+
+    if platform.system() == 'Windows':
+      cmake_args += [
+          '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)
+      ]
+      if sys.maxsize > 2**32:
+        cmake_args += ['-A', 'x64']
+      build_args += ['--', '/m']
+    else:
+      cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+      build_args += ['--', '-j2']
+
+    env = os.environ.copy()
+    env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
+        env.get('CXXFLAGS', ''), self.distribution.get_version())
+    if not os.path.exists(self.build_temp):
+      os.makedirs(self.build_temp)
+    subprocess.check_call(
+        ['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+    subprocess.check_call(
+        ['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
